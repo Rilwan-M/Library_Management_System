3ea04346273661a4524ab2e5fff32642fda7fd09
Rilwan-M <mrilwaneng@gmail.com>
2023-01-19 21:57:43 +0530

    azure setup

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,8 @@ import api.api_checkout as CheckoutApi
 app = Flask(__name__)
 
 # Set up the database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost/library'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost/library'
+app.config['SQLALCHEMY_DATABASE_URI'] = "host=bookhub-server.postgres.database.azure.com port=5432 dbname=bookhub user=bookhubadmin@bookhub-server password=assignment@1234 sslmode=require"
 db = SQLAlchemy(app)
 CORS(app)
 
