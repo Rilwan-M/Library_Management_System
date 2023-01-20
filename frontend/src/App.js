9d9407d5949816dca0fbb7662cdd3207180f30c7
Rilwan-M <mrilwaneng@gmail.com>
2023-01-20 10:18:42 +0530

    solved conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import Sidebar from "./components/Sidebar/Sidebar";
 
-
 // component imporitng
 import GetStarted from "./components/Pages/GetStarted";
 
@@ -15,26 +14,49 @@ import Login from "./components/Pages/Login";
 import Register from "./components/Pages/Register";
 
 import { Route, Routes } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div className="App">
       <div className="AppGlass">
-        <div>
-          <Sidebar />
-        </div>
+        <div>{isLoggedIn ? <Sidebar /> : null}</div>
+
+        {/* <sidebar /> */}
         <div className="table">
           <Routes>
-            <Route exact path="/" element={<GetStarted />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/register" element={<Register/>}/>
+            <Route
+              exact
+              path="/"
+              element={<GetStarted setIsLoggedIn={setIsLoggedIn} />}
+            />
+
+            <Route
+              path="/login"
+              element={<Login setIsLoggedIn={setIsLoggedIn} />}
+            />
+            <Route
+              path="/register"
+              element={<Register setIsLoggedIn={setIsLoggedIn} />}
+            />
+            {}
+            <Route
+              path="/dashboard"
+              element={
+                <Dashboard
+                  setIsLoggedIn={() => {
+                    setIsLoggedIn(true);
+                  }}
+                />
+              }
+            />
             <Route path="/books" element={<BookDetails />} />
             <Route path="/users" element={<UserDetails />} />
             <Route path="/checkout" element={<CheckoutDetails />} />
             <Route path="/addbook" element={<AddBook />} />
             <Route path="/books/:id" element={<EditBook />} />
-            
+
             {/* <Route path="/listmedicines" element={<ListMedicine />} />
             <Route path="/form" element={<Form />} />
             <Route path="/updatedetails" element={<UpdateDetails />} />
